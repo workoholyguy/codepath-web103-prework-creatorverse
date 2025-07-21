4accfdafd0e49863535d31ae502e25b903d87e54
Omar Madjitov <95881196+workoholyguy@users.noreply.github.com>
2025-07-20 22:15:54 -0400

    Adding the Project Scaffolding

--- src/client.tsx
+++ src/client.tsx
@@ -1,6 +1,7 @@
 import { createClient } from "@supabase/supabase-js";
 
-const URL = "https://nqsgrytwnxfzmntpvyry.supabase.co";
-const API_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im5xc2dyeXR3bnhmem1udHB2eXJ5Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTI5NDAxMDUsImV4cCI6MjA2ODUxNjEwNX0.2qNg9jispSKmIa_7jnIR5IGTQZDpZjguLd6AqUxcUUg";
 
-export const supabase = createClient(URL, API_KEY);
+const URL = import.meta.env.VITE_SUPABASE_PROJECT_URL;
+const ANON_KEY = import.meta.env.VITE_SUPABASE_ANON_KEY;
+
+export const supabase = createClient(URL, ANON_KEY);
